79a93ffa276a460b4db5745aa445938ed9a6e4ff
HenrySTEM <64991623+HenrySTEM@users.noreply.github.com>
2025-02-02 15:54:03 -0500

    Update Homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="Page">
             <h1>About</h1>
             <p> 
-                I am currently an twelfth grader at Detroit Country Day school. 
+                I am currently a twelfth grader at Detroit Country Day school. 
             </p>
             <p>
                 I enjoy doing competition math and competitive programming. I am also interested in theoretical math, particularly combinatorics. I post frequently 
