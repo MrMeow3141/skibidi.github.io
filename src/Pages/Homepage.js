2b04fdb559948e2578549b62a7f066eb354ef037
HenrySTEM <64991623+HenrySTEM@users.noreply.github.com>
2025-02-02 15:50:50 -0500

    Update Homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -3,14 +3,14 @@ export default function Home() {
         <div className="Page">
             <h1>About</h1>
             <p> 
-                I am currently an tenth grader at Detroit Country Day school. 
+                I am currently an twelfth grader at Detroit Country Day school. 
             </p>
             <p>
                 I enjoy doing competition math and competitive programming. I am also interested in theoretical math, particularly combinatorics. I post frequently 
                 on the competition math website <a href="artofproblemsolving.com/community/user/404771" target="_blank">Art of Problem Solving</a>.
             </p>
             <p>
-                I played the piano and flute for a few years. My hobbies include, playing chess, and playing tennis. I also enjoy solving math and logic puzzles.
+                I play the flute and I played the piano for a few years. My hobbies include, playing chess, playing tennis, and doing trivia puzzles. I also enjoy solving math and logic puzzles.
             </p>
         </div>
     )
